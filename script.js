23e8dfd78ae8cc7297a678db7d8d971096f3ba4b
FRANCO1357 <fmelani13@gmail.com>
2022-07-20 16:25:56 +0200

    email in page

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ Vue.config.devtools = true;
 const app = new Vue({
     name: 'VueEmailList',
     el: '#root',
-    data: {},
+    data: {
+        email: '',
+    },
     method: {},
     created(){
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then((res) => {
             console.log(res.data);
+            this.email = res.data.response;
         })
 
     }
